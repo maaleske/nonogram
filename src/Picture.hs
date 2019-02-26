7ebb748d223d95d1cb0858daa4c974eaeabd98f3
Matti Eskelinen <matti.a.eskelinen@student.jyu.fi>
2019-02-26 19:07:57 +0200

    Use Squares in Picture

--- src/Picture.hs
+++ src/Picture.hs
@@ -1,33 +1,34 @@
 module Picture where
+import Hints
 
 -- Picture size and list of booleans for each pixel in the image marking the state
 data Picture = Picture {
    height :: Int
   ,width :: Int
-  ,pixels :: [Bool]
+  ,pixels :: [Square]
   } deriving (Eq)
 
 instance Show (Picture) where
     show = showPic
 
 showPic :: Picture -> String
-showPic (Picture 1 _ px) = map showPx px
-  where showPx True = '█'
-        showPx False = '·'
+showPic (Picture 1 _ px) = concatMap show px
 showPic pic@(Picture h w px) = unlines . map showPic $ map (row pic) [1..h]
 
 emptyPicture :: Int -> Int -> Picture
-emptyPicture h w = Picture h w . take (h * w) $ repeat False
+emptyPicture h w = Picture h w . take (h * w) $ repeat Blank
 
 fullPicture :: Int -> Int -> Picture
-fullPicture h w = Picture h w . take (h * w) $ repeat True
+fullPicture h w = Picture h w . take (h * w) $ repeat Filled
 
 readPicture :: String -> Picture
 readPicture s = Picture h w px
-    where px = map (=='█') . concat $ rows
+    where px = map readPx . concat $ rows
           rows = lines s
           w = length . head $ rows
           h = length rows
+          readPx '█' = Filled
+          readPx '·' = Blank
 
 rowsUntil :: Picture -> Int -> Picture
 rowsUntil (Picture h w px) n = Picture (h - n + 1) w . take (n * w) $ px
