0009c0ac9e83e00da631d645e14e69113c3f31ce
Matti Eskelinen <matti.a.eskelinen@student.jyu.fi>
2019-03-25 21:19:02 +0200

    Fix error of copies and pastes

--- src/Hints/IntegerDivision.hs
+++ src/Hints/IntegerDivision.hs
@@ -11,7 +11,7 @@ solvePossible w h hhints vhints = intersect imsh imsv
 possibleImages :: Int -> [Hints] -> [[[Square]]]
 possibleImages _ [] = [[]]
 possibleImages l (h:hs) = do
-    line <- possibleLines' l h
+    line <- possibleLines l h
     rest <- possibleImages l hs
     pure $ line : rest
 
@@ -20,15 +20,6 @@ possibleLines l hints =
     let allowedEmpty = (1 >=) . abs . (length hints -) . length in
     do
         emptys <- filter allowedEmpty $ divideToNats (l - sum hints)
-        lines <- possibleLinesL hints emptys ++ possibleLinesR hints emptys
-        pure lines
-
-possibleLines' :: Int -> Hints -> [[Square]]
-possibleLines' l hints = let
-    nemptys = let nh = length hints in [nh - 1, nh, nh + 1]
-    possibleEmptys = concatMap (flip divideToKNats (l - sum hints)) nemptys
-    in do
-        emptys <- possibleEmptys
         lines <- possibleLinesL hints emptys ++ possibleLinesR hints emptys
         pure lines
 
@@ -53,25 +44,6 @@ nBlank = flip replicate Blank
 
 nFilled :: Int -> [Square]
 nFilled = flip replicate Filled
-
-memo2 :: (Int -> Int -> a) -> [[a]]
-memo2 f = map (\x -> map (f x) [0..]) [0..]
-
-divideToKNats :: Int -> Int -> [[Int]]
-divideToKNats k n = knDivisions !! k !! n
-
-knDivisions :: [[[[Int]]]]
-knDivisions = memo2 divideToKNats'
-
-divideToKNats' :: Int -> Int -> [[Int]]
-divideToKNats' 0 0 = [[]]
-divideToKNats' 0 _ = []
-divideToKNats' _ 0 = []
-divideToKNats' 1 n = [[n]]
-divideToKNats' k n = do
-    m <- [1..n]
-    rest <- divideToKNats (k-1) (n-m)
-    pure $ m : rest
 
 divideToNats :: Int -> [[Int]]
 divideToNats = (map divideToNats' [0..] !!)
